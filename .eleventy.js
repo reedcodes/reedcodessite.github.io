2af2d98644d7f895a516350c239f32b1d22377e1
Reed Piernock <18013388+reedcodes@users.noreply.github.com>
2021-11-03 18:12:58 -0400

    update colors, cleanup directories

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,12 @@ module.exports = function( eleventyConfig ) {
   const eleventyNavigationPlugin = require( '@11ty/eleventy-navigation' );
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
+  // Send assets from source to site.
+  eleventyConfig.addPassthroughCopy( {
+    "./node_modules/@fortawesome/fontawesome-free/webfonts/": "dist/webfonts/fa/",
+    "./src/_images/": "dist/images/"
+  } );
+
   // Configuration.
 
   return {
--- src/_data/colors.js
+++ src/_data/colors.js
@@ -1,26 +1,32 @@
 module.exports = [
   {
-    name: 'primary',
-    color: 'rgba(0, 121, 140, 1)'
+    // beige
+    name: "main",
+    color: "#f4f4e1"
   },
   {
-    name: 'secondary',
-    color: 'rgba(125, 59, 106, 1)'
+    // black-green
+    name: "contrast",
+    color: "#173c39"
   },
   {
-    name: 'alert',
-    color: 'rgba(208, 55, 17, 1)'
+    // straw
+    name: "highlight",
+    color: "#ccce7a"
   },
   {
-    name: 'highlight',
-    color: 'rgba(246, 174, 45, 1)'
+    // rust
+    name: "primary",
+    color: "#c43121"
   },
   {
-    name: 'light',
-    color: 'rgba(248, 245, 242, 1)'
+    // teal
+    name: "secondary",
+    color: "#95caba"
   },
   {
-    name: 'dark',
-    color: 'rgba(37, 41, 53, 1)'
+    // dark beige
+    name: "accent",
+    color: "#bea467",
   }
 ];
