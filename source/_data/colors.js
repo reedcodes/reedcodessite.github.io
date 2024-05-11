79e2cff8d6ec71954b86aa56f815a2c73ba9c1f7
Reed Piernock <agile.plan2042@snooble.com>
2024-05-11 16:50:08 -0400

    update build scripts

---
+++ source/_data/colors.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    // aquamarine
+    name: "primary",
+    color: "#8dffe2"
+  },
+  {
+    // hot pink
+    name: "secondary",
+    color: "#ff8de5"
+  },
+  {
+    // yellow
+    name: "tertiary",
+    color: "#fefb94"
+  },
+  {
+    // electric blue
+    name: "supplementary ",
+    color: "#1ae8ff"
+  }
+];
