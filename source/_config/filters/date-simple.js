2185dda40880d800c642e0a3189195a2f23bbbae
Reed Piernock <agile.plan2042@snooble.com>
2024-06-30 18:34:54 -0400

    add natural language for date in blog post meta

--- source/_config/filters/date-simple.js
+++ source/_config/filters/date-simple.js
@@ -1,8 +1,10 @@
 // Import date methods.
 const {DateTime} = require('luxon');
 
-module.exports = function(dateObj) {
+module.exports = function(dateObj, dateType) {
+  const dateFormat = dateType == 'natural' ? 'dd MMMM yyyy' : 'yyyy-MM-dd';
+
   return DateTime
     .fromISO(dateObj)
-    .toFormat('yyyy-MM-dd');
+    .toFormat(dateFormat);
 };
